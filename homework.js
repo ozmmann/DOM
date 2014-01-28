b43160d56a66b504d77c17c7b30180de9bfee621
oramazanov <oramazanov@OneIncSystems.com>
2014-01-28 17:39:13 +0200

    Initial commit

--- homework.js
+++ homework.js
@@ -3,14 +3,22 @@
  * @param {Event} e событие клика
  * @private
  */
-function _onMouseClick(e) {}
+function _onMouseClick(e) {
+    var el = e.target;
+    openPopupFromLink(el);
+}
 
 /**
  * Получает данные из ссылки
  * на основе этих данных создаёт попап (через createPopup) и добавляет его в DOM
  * @param {HTMLElement} link Ссылка с data-аттрибутами
  */
-function openPopupFromLink(link) {}
+function openPopupFromLink(link) {
+    var title = link.title, message = link.message, onOk, popup;
+    //popup = createPopup(title, message, onOk);
+    link.appendChild('<div class="popup"></div>');
+
+}
 
 /**
  * Создаёт DOM-узел с сообщением
@@ -19,4 +27,6 @@ function openPopupFromLink(link) {}
  * @param {Function} onOk Обработчик клика по кнопке 'Да'
  * @returns {HTMLElement}
  */
-function createPopup(title, message, onOk) {}
+function createPopup(title, message, onOk) {
+    return;
+}
